b41369464929704b309a4bf75077f8886870967f
samgevorgyan <Gorisigort2>
2020-03-02 14:58:01 +0100

    dd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'son-yan-denimg';
+  title = 'son-yan-ddasdasdasdensadsadimg';
 }
